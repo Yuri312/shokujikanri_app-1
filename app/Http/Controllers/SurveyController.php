bffe7587a004fc5428eb4f376de5e8ccd4a7450d
Yuri312 <100987024+Yuri312@users.noreply.github.com>
2022-04-02 14:29:54 +0900

    いったんpush

--- app/Http/Controllers/SurveyController.php
+++ app/Http/Controllers/SurveyController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Survey;
 
 class SurveyController extends Controller
 {
-    //
+    function index(){
+    #Surveyに入っているデータを全部取得
+    $surveys = Survey::all();
+    return view("surveys.index", compact("surveys"));
+    }
 }
--- database/seeds/SurveyTableSeeder.php
+++ database/seeds/SurveyTableSeeder.php
@@ -1,8 +1,10 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Survey;
 
 class SurveyTableSeeder extends Seeder
+
 {
     /**
      * Run the database seeds.
@@ -11,6 +13,6 @@ class SurveyTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Survey::create(['SURVEY_NAME' => "テスト調査", 'TERM' => "2022年4月～6月", 'ERA' => "10代～20代", 'SEX' => "男性", 'SPORT' => "陸上競技", 'REGISTER_DATE' => date("Y-m-d H:i:s"), 'REGISTER_USER_ID' => 111111, 'UPDATE_DATE' => date("Y-m-d H:i:s"), 'UPDATE_USER_ID' => 111111]);
     }
 }
